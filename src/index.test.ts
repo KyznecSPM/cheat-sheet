351864e59b196e655d14c24926742a10d73ce90b
emelyanovdo <emelyanovdo@ursip.ru>
2021-04-02 16:16:49 +0300

    add: addition string

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,13 @@
-import { hasOwnProperty } from './index'
+import { hasOwnProperty, makeKey } from './index'
 
-test('check if object has property name', () => {
+test('check object has key', () => {
   const testObj = { test: 'test' }
   expect(hasOwnProperty(testObj, 'test')).toBe(true)
   expect(hasOwnProperty(testObj, 'any')).toBe(false)
+})
+
+test('check addition string', () => {
+  const stringA = `namespace` as const
+  const stringB = `moduleName` as const
+  expect(makeKey(stringA, stringB)).toBe(`namespace-moduleName`)
 })
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 /**
- *
+ * Проверяет наличие свойства объекта
  * @param obj объект
  * @param prop проверяемое название свойства объекта
- * @returns
+ * @returns boolean
  */
 
 export function hasOwnProperty<X extends {}, Y extends PropertyKey>(obj: X, prop: Y): obj is X & Record<Y, unknown> {
   return {}.hasOwnProperty.call(obj, prop)
+}
+
+/**
+ * Создаем новую строку (компилятор TS видит возвращаемое значение как String Literal Types)
+ * @param namespace
+ * @param name
+ * @returns составная шаблонная строка namespace-name
+ */
+
+export function makeKey<NS extends string, N extends string>(namespace: NS, name: N): `${NS}-${N}` {
+  return `${namespace}-${name}` as `${NS}-${N}`
 }
