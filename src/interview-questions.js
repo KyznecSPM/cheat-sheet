62b2086007665e45ead25d89b6ec626981454f50
emelyanovdo <emelyanovdo@ursip.ru>
2021-04-05 13:59:08 +0300

    add: interview question

---
+++ src/interview-questions.js
@@ -0,0 +1,6 @@
+// 7 примитивных типов JS
+// строка, число, null, undefined, булево, символ, целые числа
+const primitiveTypes = ['string', 'number', 'null', 'undefined', 'boolean', 'symbol', 'bigint']
+
+// ложные значения
+const falsyValues = ['', 0, null, undefined, NaN, false]
